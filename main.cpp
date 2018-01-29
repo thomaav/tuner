45cd26bc2b68c17110d95e2d82e212be215945c2
Thomas Aven <avensin@hotmail.com>
2018-01-29 11:48:20 +0100

    Move getting PCM handle to own function.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <alsa/asoundlib.h>
 
-int main(int argc, char **argv)
+constexpr char pcm_name[] = "default";
+constexpr snd_pcm_format_t format = SND_PCM_FORMAT_S16_LE; // signed 16-bit little endian
+
+snd_pcm_t *get_pcm_capture_handle()
 {
 	int err;
 
@@ -9,17 +12,14 @@ int main(int argc, char **argv)
 
 	snd_pcm_t *capture_handle;
 	snd_pcm_hw_params_t *hw_params;
-	snd_pcm_format_t format = SND_PCM_FORMAT_S16_LE; // signed 16-bit little endian
 
 	/* open PCM */
-	if ((err = snd_pcm_open(&capture_handle, argv[1], SND_PCM_STREAM_CAPTURE, 0)) < 0) {
+	if ((err = snd_pcm_open(&capture_handle, pcm_name, SND_PCM_STREAM_CAPTURE, 0)) < 0) {
 		fprintf(stderr, "Unable to open audio device %s (%s)\n",
-			argv[1],
+			pcm_name,
 			snd_strerror(err));
 		exit(1);
 	}
-
-	printf("Audio interface %s\ opened\n", argv[1]);
 
 	/* allocate PCM hardware parameters */
 	if ((err = snd_pcm_hw_params_malloc(&hw_params)) < 0) {
@@ -28,16 +28,12 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	printf("Hardware params allocated\n");
-
 	/* initialize the PCM hardware parameters */
 	if ((err = snd_pcm_hw_params_any(capture_handle, hw_params)) < 0) {
 		fprintf(stderr, "Unable to initialize hw parameter structure (%s)\n",
 			snd_strerror(err));
 		exit(1);
 	}
-
-	printf("Hardware params initialized\n");
 
 	/* set the access mode of the hw parameters */
 	if ((err = snd_pcm_hw_params_set_access(capture_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
@@ -47,8 +43,6 @@ int main(int argc, char **argv)
 
 	}
 
-	printf("Hardware params access mode set\n");
-
 	/* set the sample format of the hardware parameters */
 	if ((err = snd_pcm_hw_params_set_format(capture_handle, hw_params, format)) < 0) {
 		fprintf(stderr, "Unable to set sample format (%s)\n",
@@ -56,8 +50,6 @@ int main(int argc, char **argv)
 		exit(1);
 
 	}
-
-	printf("Hardware sample format set\n");
 
 	/* set sampling rate */
 	if ((err = snd_pcm_hw_params_set_rate_near(capture_handle, hw_params, &sampling_rate, 0)) < 0) {
@@ -67,8 +59,6 @@ int main(int argc, char **argv)
 
 	}
 
-	printf("Hardware sample rate set\n");
-
 	/* set sampling channels */
 	if ((err = snd_pcm_hw_params_set_channels(capture_handle, hw_params, 2)) < 0) {
 		fprintf(stderr, "Unable to set channel count (%s)\n",
@@ -76,8 +66,6 @@ int main(int argc, char **argv)
 		exit(1);
 
 	}
-
-	printf("Hardware sample channel count set\n");
 
 	/* actually set the hw_params */
 	if ((err = snd_pcm_hw_params(capture_handle, hw_params)) < 0) {
@@ -87,10 +75,16 @@ int main(int argc, char **argv)
 
 	}
 
-	printf("Hardware parameters set\n");
-
 	snd_pcm_hw_params_free(hw_params);
-	printf("hw_parms freed\n");
 
 	printf("Audio interface prepared\n");
+
+	return capture_handle;
+}
+
+int main(int argc, char *argv[1])
+{
+	snd_pcm_t *pcm_handle = get_pcm_capture_handle();
+
+	char *buffer = new char[128 * snd_pcm_format_width(format) / 2 * 2];
 }
