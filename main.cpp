077f018906226f1f4dfad7e05c945d58076357bd
Thomas Aven <avensin@hotmail.com>
2018-02-03 14:12:57 +0100

    Trap SIGINT and be more user friendly.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,53 @@
 #include <alsa/asoundlib.h>
+#include <signal.h>
 
 #include "capture.h"
 #include "wav.h"
 #include "fft.h"
 #include "notes.h"
 
+static volatile int run = 1;
+
+void enable_cursor()
+{
+	printf("\e[?25h");
+	fflush(stdout);
+}
+
+void disable_cursor()
+{
+	printf("\e[?25l");
+	fflush(stdout);
+}
+
+void sigint(int _)
+{
+	run = 0;
+}
+
 int main(int argc, char *argv[1])
 {
 	size_t samples_per_period = 11025;
+	double peak;
 
 	PCMDevice pcm_device{};
 	short *chunk = new short[samples_per_period * snd_pcm_format_width(format) / 8];
 
-	for ( ;; ) {
+	disable_cursor();
+	signal(SIGINT, sigint);
+	printf("\n\n\n");
+
+	while (run) {
 		pcm_device.get_samples(chunk, samples_per_period);
-		printf("%f\n", fft_median_peak_frequency(chunk, samples_per_period));
+		peak = fft_median_peak_frequency(chunk, samples_per_period);
+
+		if (peak < 60.0)
+			peak = 0.0;
+
+		printf("Peak frequency: %f\r", peak);
+		fflush(stdout);
 	}
 
+	enable_cursor();
 	free(chunk);
 }
